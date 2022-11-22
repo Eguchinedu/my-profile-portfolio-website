cbceab757b63d1b18ea4e485c0b65dc7c7c4f918
Egu Chinedu <eguchinedu18@gmail.com>
2022-11-22 03:51:15 +0100

    added project images

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,44 +1,54 @@
-import pro1 from "../assets/project1.png"
-import pro2 from "../assets/project2.png"
-import pro3 from "../assets/project3.png"
+import apextalents from "../assets/apextalents.png"
+import calculator from "../assets/calculator.png"
+import food from "../assets/food-site.png"
+import phonie from "../assets/phonie.png"
+import portfolio from "../assets/portfolio-fetch.png"
+import store from "../assets/store-website.png"
+import travel from "../assets/travel.png"
 
 
 const ProjectCardData = [
   {
-    imgsrc: pro1,
+    imgsrc: phonie,
     title: "Cloud-278-phonie",
     text: "The form created in this page detects the carrier of the number provided and displays the logo. this is created using HTML, CSS and Javascript",
     view: "https://cloud-278-phonie.repl.co/",
   },
   {
-    imgsrc: pro2,
+    imgsrc: calculator,
     title: "React Calculator",
     text: "This fully functional calculator was created using React.js. it accepts inputs and displays the results ",
     view: "https://q2m5tc.csb.app/",
   },
   {
-    imgsrc: pro3,
+    imgsrc: apextalents,
     title: "ApexTalents",
     text: "This is a tech recruitment website i created with Word Press While working with ApexTalents, also with my Knowledge of HTML and CSS",
     view: "http://apextalents.net/",
   },
   {
-    imgsrc: pro3,
+    imgsrc: food,
     title: "Food Site",
     text: "This was created with vanilla html and css 3 months in a coding boot camp by me in collaboration with my collegues, i created the home page while they worked on the rest.",
     view: "https://food-site-page.chineduegu.repl.co/",
   },
   {
-    imgsrc: pro3,
+    imgsrc: portfolio,
     title: "Github-Profile-Fetch",
     text: "This was created Using React.js, it fetches my github profile and repos, also contains error boundary and 404 page.",
     view: "https://github-portfolio-fetch.web.app/",
   },
   {
-    imgsrc: pro3,
+    imgsrc: store,
     title: "Store Website",
     text: "This is a prototype e-commerce website that i created using HTML, CSS and Javascript",
     view: "https://store-website.chineduegu.repl.co/",
+  },
+  {
+    imgsrc: travel,
+    title: "Travel Website",
+    text: "This is a simple prototype Travel website that i created using React.js.",
+    view: "https://travel-website-8600a.web.app",
   },
 ];
 
